d97d474e76039de1b8755dc46c6e89b68a1d15d3
Majboos-Laham <160101680+Majboos-Laham@users.noreply.github.com>
2024-02-15 06:53:14 +0000

    Student Card

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Card from "./components/Card";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card />
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
