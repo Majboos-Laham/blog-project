343619e83b53893dcb4a78e319fdb167db7d1fca
Majboos-Laham <160101680+Majboos-Laham@users.noreply.github.com>
2024-04-17 10:42:14 +0000

    Revert "25 - Profile page & 26 - Update user profile"
    This reverts commit c884ffc927c337300cb05fd1c596566899689048.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import {Toaster} from 'react-hot-toast'
 
 import "./App.css";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
 import RegisterPage from "./pages/register/RegisterPage";
 import LoginPage from "./pages/login/LoginPage";
-import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
       </Routes>
       <Toaster />
     </div>
